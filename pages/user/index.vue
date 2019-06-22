784fecefd99c61e5b6800777d255e8af9cbf2329
Ei Hirai <ei.nabra.development@gmail.com>
2019-06-22 14:40:49 +0900

    Start Nuxt.js project for lecture3-4

---
+++ pages/user/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>This is my first page for mec lecture</h1>
+      <div>
+        <nuxt-link to="/">ホームページに戻る</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
