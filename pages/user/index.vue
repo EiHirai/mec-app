685055a50629b98e3358fb3f94ea8cc36f46fdd5
Ei Hirai <ei.nabra.development@gmail.com>
2019-06-22 16:24:34 +0900

    create Nuxt app

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
-    <div>
+    <div class="row">
       <h1>This is my first page for mec lecture</h1>
+      <include src="../assets/src/mapping_sample_10.html"></include>
+    </div>
+    <div class="row">
       <div>
         <nuxt-link to="/">ホームページに戻る</nuxt-link>
       </div>
